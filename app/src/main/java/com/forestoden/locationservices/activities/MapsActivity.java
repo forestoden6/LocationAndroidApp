77fc74fc5b1656c890f8b491d715e5765f9e90b5
Steve Kravitsky <drago509_6@hotmail.com>
2016-11-15 13:25:34 -0500

    Test Commit

--- app/src/main/java/com/forestoden/locationservices/activities/MapsActivity.java
+++ app/src/main/java/com/forestoden/locationservices/activities/MapsActivity.java
@@ -40,6 +40,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
+        //Test commit
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
